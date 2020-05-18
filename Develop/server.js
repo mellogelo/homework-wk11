ea05bdf51ff3768af7447c6a3571dad0f0cec948
Ralph Magbanua <rmagbanua09@gmail.com>
2020-05-18 00:28:50 -0700

    updated server

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,14 @@
 const express = require("express");
 // const path = require("path");
-const htmlRoutes = require("./routes/htmlRoutes");
-const apiRoutes = require("./routes/apiRoutes")
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
-app.use("/api", apiRoutes );
-app.use("/", htmlRoutes );
+
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
 
 app.listen(PORT, () => 
     console.log("Listening on Port: " + PORT + "." )
