55385e45a00923a3c5fdbe5671e87672f8ff68b1
Ralph Magbanua <rmagbanua09@gmail.com>
2020-06-11 23:26:31 -0700

    qwerwe

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,10 +3,10 @@ var path =require("path");
 
 
 router.get("/notes", function(req, res){
-    res.sendFile(path.join(__dirname,"notes.html"))
+    res.sendFile(path.join(__dirname,"../public/notes.html"))
 })
 router.get("*", function(req, res){
-    res.sendFile(path.join(__dirname,"index.html"))
+    res.sendFile(path.join(__dirname,"../public/index.html"))
 })
 
 module.exports = router
